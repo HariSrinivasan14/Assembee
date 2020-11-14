5614271fb071654daa0731a79c4b8dbf45d232c0
Hari Srinivasan <hari.srinivasan@mail.utoronto.ca>
2020-11-14 13:36:58 -0500

    created a activity for searching projects

--- Project/app/src/main/java/com/example/assembee/explore_dummy.kt
+++ Project/app/src/main/java/com/example/assembee/explore_dummy.kt
@@ -1,10 +1,13 @@
 package com.example.assembee
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.SearchView
+import android.widget.Toast
 
 /**
  * A simple [Fragment] subclass.
@@ -15,9 +18,35 @@ class explore_dummy : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
+
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_explore_dummy, container, false)
+
+        val result = inflater.inflate(R.layout.fragment_explore_dummy, container, false)
+        val searchView = view?.findViewById<SearchView>(R.id.search_view)
+        searchView?.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                val projectname: String? =  query
+                val intent = Intent(activity, search_project::class.java)
+                intent.putExtra("ProjectName", projectname)
+                startActivity(intent)
+                return true
+            }
+
+            //            override fun onQueryTextSubmit(query: String): Boolean {
+//                if (list.contains(query)) {
+//                    adapter.filter.filter(query)
+//                } else {
+//                    Toast.makeText(this@MainActivity, "No Match found", Toast.LENGTH_LONG).show()
+//                }
+//                return false
+//            }
+            override fun onQueryTextChange(newText: String): Boolean {
+
+                return false
+            }
+        })
+        return result
     }
 
     companion object {
