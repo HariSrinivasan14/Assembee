8337dce94fb24d3eb56931aa565fd0a93457e55c
Scott Shen <scottshen0515@gmail.com>
2020-11-29 11:58:41 -0500

    Added accept layout

---
+++ Project/app/src/main/java/com/example/assembee/AcceptJoinRequest.java
@@ -0,0 +1,14 @@
+package com.example.assembee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AcceptJoinRequest extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_accept_join_request);
+    }
+}
